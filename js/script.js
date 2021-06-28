e4082e41bda0ff4dc8235112f1e1b277ce02107a
Alex <lekschannel1@gmail.com>
2021-06-28 22:13:25 +0500

    1st part of practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-"smth"
+const numberOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?", ""));
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Фильм", ""),
+      b = prompt("Оценка?", ""),
+      c = prompt("Фильм", ""),
+      d = prompt("Оценка?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
